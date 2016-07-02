05aa2efbb1fc8169541b721cfa70d046c881a13f
lelouch0823 <lelouch0823@hotmail.com>
2016-07-02 16:30:52 +0800

    替换Fragment

--- FragmentTest/app/src/main/java/com/bjw/fragmenttest/MainActivity.java
+++ FragmentTest/app/src/main/java/com/bjw/fragmenttest/MainActivity.java
@@ -1,20 +1,38 @@
 package com.bjw.fragmenttest;
 
-import android.content.DialogInterface;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener{
+import com.orhanobut.logger.Logger;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
 
     }
 
     @Override
-    public void onClick(DialogInterface dialogInterface, int i) {
-
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                Logger.d("hello");
+                AnotherRightFragment anotherRightFragment = new AnotherRightFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.right_layout, anotherRightFragment);
+                transaction.commit();
+                break;
+            default:
+                break;
+        }
     }
 }
