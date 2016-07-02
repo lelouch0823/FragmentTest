188ce2b701bb7581eaaa281e03cb09fde126717d
lelouch0823 <lelouch0823@hotmail.com>
2016-07-03 00:10:06 +0800

    News app

---
+++ FragmentBestPractice/app/src/main/java/com/bjw/fragmentbestpractice/News.java
@@ -0,0 +1,34 @@
+package com.bjw.fragmentbestpractice;
+
+/**
+ * Created by Administrator on 2016/7/2 0002.
+ */
+public class News {
+    private String title;
+    private String content;
+
+    public News(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public News() {
+    }
+
+    public String getContent() {
+
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
