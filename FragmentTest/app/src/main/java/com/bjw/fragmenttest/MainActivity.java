8fd8b2f3c2541b94fbb5ded4c8652b710cb95984
lelouch0823 <lelouch0823@hotmail.com>
2016-07-02 10:01:18 +0800

    20160702

--- FragmentTest/app/src/main/java/com/bjw/fragmenttest/MainActivity.java
+++ FragmentTest/app/src/main/java/com/bjw/fragmenttest/MainActivity.java
@@ -1,13 +1,20 @@
 package com.bjw.fragmenttest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onClick(DialogInterface dialogInterface, int i) {
+
     }
 }
---
+++ FragmentTest/app/src/main/java/com/bjw/fragmenttest/RightFragment.java
@@ -0,0 +1,20 @@
+package com.bjw.fragmenttest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2016/7/2 0002.
+ */
+public class RightFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.right_fragment, container, false);
+        return view;
+    }
+}
